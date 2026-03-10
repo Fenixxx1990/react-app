702045c3bfc986ebbf1c235d921a07f8cea76b9b
Fenix <fenixxx1990@yandex.ru>
2026-03-11 01:31:55 +0400

    add cards

--- eslint.config.js
+++ eslint.config.js
@@ -26,6 +26,7 @@ export default defineConfig([
       "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
       semi: ["error", "always", { omitLastInOneLineBlock: false }],
       quotes: ["error", "double"],
+      "react/prop-types": "off",
     },
   },
 ]);
